6fcdba743057c39305534d6ba3266ae2e54e6ee0
Ghost_fz <lsta.090199@gmail.com>
2021-06-15 10:26:33 -0600

    Intercambio inicial de proyecto

---
+++ samples.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class samples here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class samples
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int n;
+    private int r;
+
+    /**
+     * Constructor for objects of class samples
+     */
+    public samples()
+    {
+        // initialise instance variables
+        x = 7;
+        n = 1;
+        r = 10;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+n+r+y;
+    }
+}
